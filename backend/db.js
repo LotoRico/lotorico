b2cb1e9f7b661106f175f2e2ea975cedecfc15cf
lotorico_old <lotorico.oficial@gmail.com>
2026-08-17 18:34:33 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,5 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL || {
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT || 3306,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  waitForConnections: true,
-  connectionLimit: 5,
-  queueLimit: 0,
-  ssl: { rejectUnauthorized: false }
-});
+const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
 module.exports = pool;
